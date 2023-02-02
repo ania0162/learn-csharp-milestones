a1d7f88cc605d89d55f7cfd500fe37e673f23cf4
ania0162 <122844125+ania0162@users.noreply.github.com>
2023-02-02 11:01:03 -0500

    Commit to Chapter 2 (from GitHub Desktop)

---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public int CurrentAge = 21;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(21 + 1);
+        Debug.Log(CurrentAge + 1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
